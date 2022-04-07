cb246c27fc30d274034cd1070fff79b21bfa6b7a
Barrett Hiltabrand <bear2927@yahoo.com>
2022-04-07 10:46:36 -0500

    commit tree 08c4f7890fc6f2694e50b4ba10fc6f2bdffb168f

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(cards, event) {
+    let newArr = [];
+    for (let i = 0; i < cards.length; i++) {
+       let message = (`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+       newArr.push(message);
+    } 
+    return newArr;
+}
+
+function countDown(int) {
+    while (int>= 0) {
+        console.log(int);
+        int--;
+    }
+}
